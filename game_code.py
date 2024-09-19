fb3a6bfce876208de3c430649a815d06b0d3e826
boulderingfreak <rsitek97@gmail.com>
2024-09-19 07:32:05 +0200

    Added Features

--- game_code.py
+++ game_code.py
@@ -7,8 +7,12 @@ print("Welcome to Madlibs Game!!! \n Word game where you create a story by filli
 adjective = input("Enter an adjective (description): ")
 noun = input("Enter a noun (place, person, thing): ")
 verb = input("Enter a verb (running,swimming etc.): ")
+adjective2 = input("Enter other adjective (description): ")
+noun2 = input("Enter a other noun (place, person, thing): ")
 
 
 print(f"Today I was in {adjective} bar with my bros.")
 print(f"My bro found such a nice {noun}.")
 print(f"So we decided to go {verb}.")
+print(f"In front of the bar {noun2} said 'Hello!' to us.")
+print(f"It was such a {adjective2} day!!!")
