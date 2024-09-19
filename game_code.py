f4e4ce22e1303e0999e96547d1bc0edbf729b4d3
boulderingfreak <rsitek97@gmail.com>
2024-09-19 07:24:07 +0200

    First Commit

---
+++ game_code.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# word game where you create a story
+# by filling in blanks with random words
+
+print("Welcome to Madlibs Game!!! \n Word game where you create a story by filling in blanks random words")
+
+adjective = input("Enter an adjective (description): ")
+noun = input("Enter a noun (place, person, thing): ")
+verb = input("Enter a verb (running,swimming etc.): ")
+
+
+print(f"Today I was in {adjective} bar with my bros.")
+print(f"My bro found such a nice {noun}.")
+print(f"So we decided to go {verb}.")
